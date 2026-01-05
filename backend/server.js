a6f31ac521d312ce6ec591edab2ced4e84fb3e1c
safsouf123 <52330617@students.liu.edu.lb>
2026-01-05 11:59:19 +0200

    fixed mysql connection it was resetting

--- backend/server.js
+++ backend/server.js
@@ -9,25 +9,22 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+//Fixed instead of createConnection i did createPool(January 5 2026)
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 3306),
 
-const db = mysql.createConnection({
-host: process.env.DB_HOST || "localhost",
-user: process.env.DB_USER || "root",
-password: process.env.DB_PASSWORD || "",
-database: process.env.DB_NAME || "bakery_db",
-port: Number(
-    process.env.DB_PORT ||
-    3306
-  ),
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    process.exit(1);
-  }
-  console.log("Database connected successfully");
-});
 
 
 app.get("/", (req, res) => res.send("ok"));
